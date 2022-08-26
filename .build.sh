92de41ae4d666f9c3105e0e77a3f2db31d999b3b
Yingbei-Tong <tybalex@gmail.com>
2022-08-25 20:12:46 -0700

    tested multi-cluster cases, update yaml file
    Signed-off-by: Yingbei-Tong <tybalex@gmail.com>

--- .build.sh
+++ .build.sh
@@ -1,9 +1,9 @@
-IMAGE_NAME=tybalex/opni-rca:mc
+IMAGE_NAME=tybalex/opni-rca:mc1
 docker build . -t $IMAGE_NAME -f ./Dockerfile
 
 docker push $IMAGE_NAME
 
-IMAGE_NAME2=tybalex/opni-rca_trainer:mc
+IMAGE_NAME2=tybalex/opni-rca_trainer:mc1
 docker build . -t $IMAGE_NAME2 -f ./Dockerfile_trainer
 
 docker push $IMAGE_NAME2
--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM python:3.8-slim
 COPY ./src ./
 RUN pip install --no-cache-dir -r ./requirements.txt
 
+EXPOSE 5050
 CMD ["uvicorn", "rca_service:app","--port"," 5050", "--reload"]
-
